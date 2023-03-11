86d8255ba68717b63ebcbbac0027b130d4bdd435
Jeremy-kr <wjddlr905@gmail.com>
2023-03-11 22:42:50 +0900

    fix: 타임아웃을 30초로 설정

--- utils/postCode.ts
+++ utils/postCode.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 
 const postCode = async (code: string) => {
-  const res = await axios.post("/api/review", { code });
+  const res = await axios.post(
+    "/api/review",
+    { code },
+    {
+      timeout: 30000,
+    }
+  );
 
   return res;
 };
